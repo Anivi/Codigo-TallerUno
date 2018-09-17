badc0b76fa359acd6445293355f1e6a3d1a226eb
anivi <anivi98@hotmail.com>
2018-09-16 19:27:42 -0500

    buenas noches jeje

--- VictoriaAnaIsabel_TallerUno/src/Bowl.java
+++ VictoriaAnaIsabel_TallerUno/src/Bowl.java
@@ -1,7 +1,8 @@
 import processing.core.*;
 
 public class Bowl {
-
+    private boolean tapa = false;
+    private int contador;
 	PApplet app;
 	String elemento;
 	public Bowl(PApplet app, String e) {
@@ -40,10 +41,37 @@ public class Bowl {
 		
 	}
 	
-	public void cerrar() {
+	public void cerrar(int x) {
 		
 		
+		if(tapa == true) {
+			
+	contador++;
+		
+		if(elemento == "Fuego") {
+			app.fill(231,37,18);
+		}else if(elemento == "Aire") {
+			app.fill(140);
+		}else if(elemento == "Agua") {
+			app.fill(59,131,189);
+		}else if(elemento == "Tierra") {
+			app.fill(166,94,46);
 		}
+		app.arc(x,600,180 , 180, app.radians(180),app.radians(360)); 
+		if(contador == 150) {
+			contador = 0;
+			tapa=false;
+		}
+		}
+	}
+	public boolean isTapa() {
+		return tapa;
+	}
+	public void setTapa(boolean tapa) {
+		this.tapa = tapa;
+	}
+	
+	
 	}
 
     
--- VictoriaAnaIsabel_TallerUno/src/Logica.java
+++ VictoriaAnaIsabel_TallerUno/src/Logica.java
@@ -25,11 +25,32 @@ public class Logica {
 	public void pintar() {
 		for(int i=0; i<bowl.size(); i++) {
 			bowl.get(i).pintar(240*(i+1));
+			bowl.get(i).cerrar(240*(i+1));
 		}
 	}
 	
 	public void teclado() {
-		
+		for (int i = 0; i < bowl.size(); i++) {
+			Bowl o = bowl.get(i);
+			if(o.isTapa()==false) {
+				if(app.key == 'f' && o.elemento == "Fuego") {
+					o.setTapa(true);
+					System.out.println("holi");
+				}
+				if(app.key == 'v' && o.elemento == "Aire") {
+					o.setTapa(true);
+					System.out.println("holi");
+				}
+				if(app.key == 'a' && o.elemento == "Agua") {
+					o.setTapa(true);
+					System.out.println("holi");
+				}
+				if(app.key == 't' && o.elemento == "Tierra") {
+					o.setTapa(true);
+					
+				}
+		}
+		}
 	}
 	public void mouse() {
 		
